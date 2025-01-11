1b4874e1c51f72413524643bcaeec251102276bf
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-11 21:12:35 +0700

    Menambahkan parameter mahasiswa value Mahasiswa

--- app/src/main/java/com/example/application10/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/application10/repository/MahasiswaRepository.kt
@@ -8,6 +8,6 @@ interface MahasiswaRepository {
     suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
     suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
-    suspend fun deleteMahasiswa(nim: String)
+    suspend fun deleteMahasiswa(nim: String, mahasiswa: Mahasiswa)
     suspend fun getMahasiswabyNim(nim: String):Flow<Mahasiswa>
 }
