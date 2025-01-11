bf40acfee71dbba6dd8033d53fbf61bbf2ace684
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-11 21:16:23 +0700

    Menambahkan fungsi menghapus data mahasiswa berdasarkan nim

--- app/src/main/java/com/example/application10/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/application10/repository/NetworkMahasiswaRepository.kt
@@ -46,8 +46,15 @@ class NetworkMahasiswaRepository(
         }
     }
 
-    override suspend fun deleteMahasiswa(nim: String) {
-        TODO("Not yet implemented")
+    override suspend fun deleteMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
+        }
     }
 
     override suspend fun getMahasiswabyNim(nim: String): Flow<Mahasiswa> {
