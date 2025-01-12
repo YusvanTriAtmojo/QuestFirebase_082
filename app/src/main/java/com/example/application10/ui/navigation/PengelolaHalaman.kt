a27bfa95138759c03da25ff3ed8fba25596d668f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-12 15:31:08 +0700

    Membuat fungsi pengelolahalaman dengan halaman awal yaitu home

---
+++ app/src/main/java/com/example/application10/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,21 @@
+package com.example.application10.ui.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+    }
+}
