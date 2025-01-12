fc419c2a102ba0a6f4254297529ff99a516971e2
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-12 12:10:16 +0700

    Membuat MahasiswaEvent yang menyimpan data input form

---
+++ app/src/main/java/com/example/application10/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.application10.ui.viewmodel
+
+
+//data class variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+
+)
