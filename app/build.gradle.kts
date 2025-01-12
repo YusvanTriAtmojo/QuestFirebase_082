77508335dbdd18ef8901c572e1e57add178dee67
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-12 15:28:10 +0700

    Menambahkan implementation navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 }
